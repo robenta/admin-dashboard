52ed6c44ae8739baa06c536169790330e3d0e2ee
robenta <robentaahimie4@gmail.com>
2023-12-22 22:03:23 +0100

    progress

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ const body = document.querySelector('body')
 const logoText = document.querySelector('.logo-text')
 const logoIcon = document.querySelector('#logo-icon')
 const mBody = document.querySelector('#main-body-col')
+const sideMenu = document.querySelector('#side-menu')
+const sideMenuSmall = document.querySelector('#side-menu-small')
 const faIcons = document.querySelectorAll('.fa-solid')
 const faReg = document.querySelectorAll('.fa-regular')
 const modes1 = document.querySelector('.modes1')
@@ -13,50 +15,88 @@ const spanName = document.querySelectorAll('.name')
 const headerImage = document.querySelectorAll('.headerImage')
 const ul = document.querySelector('#offcanvasRight')
 const navbarToggler = document.querySelector('.navbar-toggler')
-const search = document.querySelectorAll('.search')
 const allElements = document.querySelectorAll('div')
 const pTexts = document.querySelectorAll('.pTexts')
 const itemTexts = document.querySelectorAll('.itemText')
+const whiteDiv = document.querySelectorAll('.bg-white')
+const tableRows = document.querySelectorAll('.table-row')
+const clearTaskButtons2 = document.querySelectorAll('.clearTask2')
+const search = document.querySelectorAll('.search')
+const tripleIcon = document.querySelectorAll('.tripleIcon')
+// const mainBodyH5 = document.querySelectorAll('.mainBodyh5')
+const darkTexts = document.querySelectorAll('.darkTexts')
+const bodyIcon = document.querySelectorAll('.bodyIcon')
+
+
+
 
 modes1.addEventListener('click', switchModes)
 function switchModes(e){
     if (e.target.classList.contains('fa-moon')) {
         body.style.backgroundColor = 'black'
         mBody.style.backgroundColor = 'black'
-        logoText.style.color = '#beabc1'
-        logoIcon.style.color = '#beabc1'
+        logoText.style.color = '#ffd0ce'
+        logoIcon.style.color = '#ffd0ce'
         header.style.backgroundColor = 'black'
         offcanvas.style.backgroundColor = 'black'
-        offcanvas.style.borderLeftColor = '#d688ff'
-        navbarToggler.style.color = '#d688ff'
+        navbarToggler.style.color = '#ffd0ce'
+        offcanvas.style.borderLeftColor = '#ffd0ce'
         offcanvas.style.borderLeftStyle = "solid"; 
         offcanvas.style.borderLeftWidth = "0.5px";
-        topRow.classList.add("shadowAdded");
+        // sideMenu.classList.add('border-end')
+        // sideMenu.classList.add('border-dark')
+        sideMenu.style.borderRightColor = '#ffd0ce'
+        sideMenu.style.borderRightStyle = 'solid'
+        sideMenu.style.borderRightWidth = "0.1px";
+        // sideMenuSmall.style.borderRightColor = "#ffd0ce"; 
+        // sideMenuSmall.style.borderRightStyle = "solid"; 
+        // sideMenuSmall.style.borderRightWidth = "0.3px";
+        // topRow.classList.add("shadowAdded");
         faIcons.forEach((fa)=>{
-            fa.style.color = '#beabc1'
+            fa.style.color = '#ffd0ce'
         })
         faReg.forEach((fag)=>{
-            fag.style.color = '#beabc1'
+            fag.style.color = '#ffd0ce'
         })
         allElements.forEach((all)=>{
-            all.style.color = '#beabc1'
+            all.style.color = '#ffd0ce'
         })
-        search.forEach((all)=>{
-            all.style.backgroundColor = '#f5f5f5'
-            all.style.border = '2px solid #d688ff'
-            all.classList.add('dark-mode');
-        })
+        // search.forEach((all)=>{
+        //     all.style.borderBottomColor = ''
+        //     all.style.borderBottomStyle = 'solid'
+        //     all.style.borderBottomWidth = '3px'
+        // })
         spanName.forEach((all)=>{
-            all.style.color = '#beabc1'
+            all.style.color = '#ffd0ce'
         })
         pTexts.forEach((all)=>{
-            all.style.color = '#beabc1'
+            all.style.color = '#ffd0ce'
         })
         itemTexts.forEach((list)=>{
-            list.style.color = '#beabc1'
+            list.style.color = '#ffd0ce'
         })
         headerImage.forEach((all)=>{
-            all.style.border = '2px solid #beabc1'
+            all.style.border = '2px solid #ffd0ce'
+        })
+        whiteDiv.forEach((all)=>{
+            all.classList.remove('bg-white')
+            all.classList.add('bg-light')
+            
+        })
+        tableRows.forEach((all)=>{
+            all.classList.add('table-light')
+        })
+        clearTaskButtons2.forEach((all)=>{
+            all.style.color = ''
+        })
+        tripleIcon.forEach((all)=>{
+            all.style.color = '';
+        })
+        darkTexts.forEach((all)=>{
+            all.style.color = '#383838';
+        })
+        bodyIcon.forEach((all)=>{
+            all.style.color = '';
         })
         modes1.classList.remove('fa-moon')
         modes1.classList.add('fa-sun')
@@ -71,10 +111,13 @@ function switchModes(e){
         mBody.style.backgroundColor = ''
         offcanvas.style.backgroundColor = ''
         navbarToggler.style.color = ''
-        offcanvas.style.borderLeftColor = ''
-        offcanvas.style.borderLeftStyle = ""; 
-        offcanvas.style.borderLeftWidth = "";
-        topRow.classList.remove("shadowAdded");
+        // offcanvas.style.borderLeftColor = ''
+        // offcanvas.style.borderLeftStyle = ""; 
+        // offcanvas.style.borderLeftWidth = "";
+        sideMenu.style.borderRightColor = ''
+        sideMenu.style.borderRightStyle = ''
+        sideMenu.style.borderRightWidth = "";
+        // topRow.classList.remove("shadowAdded");
 
         faIcons.forEach((fa)=>{
             fa.style.color = ''
@@ -88,7 +131,6 @@ function switchModes(e){
         search.forEach((all)=>{
             all.style.backgroundColor = ''
             all.style.border = ''
-            all.classList.remove('dark-mode');
         })
         pTexts.forEach((all)=>{
             all.style.color = ''
@@ -101,6 +143,13 @@ function switchModes(e){
         })
         headerImage.forEach((all)=>{
             all.style.border = ''
+        })
+        whiteDiv.forEach((all)=>{
+            all.classList.remove('bg-light')
+            all.classList.add('bg-white')
+        })
+        tableRows.forEach((all)=>{
+            all.classList.remove('table-light')
         })
 
         modes1.classList.add('fa-moon')
@@ -117,40 +166,51 @@ function switchModes2(e){
     if (e.target.classList.contains('fa-moon')) {
         body.style.backgroundColor = 'black'
         mBody.style.backgroundColor = 'black'
-        logoText.style.color = '#beabc1'
-        logoIcon.style.color = '#beabc1'
+        logoText.style.color = '#ffd0ce'
+        logoIcon.style.color = '#ffd0ce'
         header.style.backgroundColor = 'black'
         offcanvas.style.backgroundColor = 'black'
         offcanvas.style.borderLeftColor = '#d688ff'
         navbarToggler.style.color = '#d688ff'
         offcanvas.style.borderLeftStyle = "solid"; 
         offcanvas.style.borderLeftWidth = "0.5px";
+        sideMenu.style.borderRightColor = '#ffd0ce'
+        sideMenu.style.borderRightStyle = 'solid'
+        sideMenu.style.borderRightWidth = "0.3px";
         topRow.classList.add("shadowAdded");
         faIcons.forEach((fa)=>{
-            fa.style.color = '#beabc1'
+            fa.style.color = '#ffd0ce'
         })
         faReg.forEach((fag)=>{
-            fag.style.color = '#beabc1'
+            fag.style.color = '#ffd0ce'
         })
         allElements.forEach((all)=>{
-            all.style.color = '#beabc1'
+            all.style.color = '#ffd0ce'
         })
         search.forEach((all)=>{
             all.style.backgroundColor = '#f5f5f5'
-            all.style.border = '2px solid #d688ff'
-            all.classList.add('dark-mode');
+            all.style.borderColor = ' #d688ff'
+            all.style.borderStyle = ' solid'
+            all.style.borderWidth = ' 1px'
         })
         spanName.forEach((all)=>{
-            all.style.color = '#beabc1'
+            all.style.color = '#ffd0ce'
         })
         pTexts.forEach((all)=>{
-            all.style.color = '#beabc1'
+            all.style.color = '#ffd0ce'
         })
         itemTexts.forEach((list)=>{
-            list.style.color = '#beabc1'
+            list.style.color = '#ffd0ce'
         })
         headerImage.forEach((all)=>{
-            all.style.border = '2px solid #beabc1'
+            all.style.border = '2px solid #ffd0ce'
+        })
+        whiteDiv.forEach((all)=>{
+            all.classList.remove('bg-white')
+            all.classList.add('bg-light')
+        })
+        tableRows.forEach((all)=>{
+            all.classList.add('table-light')
         })
         modes2.classList.remove('fa-moon')
         modes2.classList.add('fa-sun')
@@ -181,7 +241,6 @@ function switchModes2(e){
         search.forEach((all)=>{
             all.style.backgroundColor = ''
             all.style.border = ''
-            all.classList.remove('dark-mode');
         })
         pTexts.forEach((all)=>{
             all.style.color = ''
@@ -194,6 +253,13 @@ function switchModes2(e){
         })
         headerImage.forEach((all)=>{
             all.style.border = ''
+        })
+        whiteDiv.forEach((all)=>{
+            all.classList.remove('bg-light')
+            all.classList.add('bg-white')
+        })
+        tableRows.forEach((all)=>{
+            all.classList.remove('table-light')
         })
 
         modes2.classList.add('fa-moon')
@@ -209,8 +275,6 @@ function switchModes2(e){
 const topRow = document.querySelector('#top-row')
 const menuIcon = document.querySelector('#menu-icon')
 const mainRow = document.querySelector('#main-row')
-const sideMenu = document.querySelector('#side-menu')
-const sideMenuSmall = document.querySelector('#side-menu-small')
 const mainBody = document.querySelector('#main-body')
 
 menuIcon.addEventListener('click', hideFirstColumn)
@@ -278,7 +342,7 @@ const offcanvasToggle = document.querySelector('#offcanvasToggle')
 offcanvasToggle.addEventListener('click', dimHeaderTextColor)
 function dimHeaderTextColor(){
     if (offcanvasChat.classList.contains('d-none')) {
-        chats.style.color = '#d5d3d3'
+        chats.style.color = 'grey'
     }else{
         chats.style.color = 'white'
     }
@@ -300,7 +364,7 @@ function showTodo(){
     todoList.style.color = 'white'
 }
 
-// TODO LIST
+// OFF-CANVAS TODO LIST
 const submitBtn = document.querySelector('#addTask')
 const inputDiv = document.querySelector('#inputDiv')
 const input = document.querySelector('#todoInput')
@@ -373,10 +437,274 @@ function toggleItemText(checkbox) {
 function showAlert(message, className){
     const alertDiv = document.createElement('div')
     alertDiv.className = `alert alert-${className}`
-    alertDiv.appendChild(document.createTextNode(message))
+    alertDiv.append(document.createTextNode(message))
     offcanvasTodo.insertBefore(alertDiv, inputDiv)
 
     setTimeout(()=> document.querySelector('.alert').remove(), 2000)
 }
 
 
+// MAIN-BODY TODO LIST
+const submitBtn2 = document.querySelector('#addTask2')
+const inputDiv2 = document.querySelector('#inputDiv2')
+const input2 = document.querySelector('#todoInput')
+const itemList2 = document.querySelectorAll('.itemText2')
+const checkboxes2 = document.querySelectorAll('.checkbox2')
+// const clearTaskButtons2 = document.querySelectorAll('.clearTask2')
+const list2 = document.querySelector('.todoList2')
+const todoList2 = document.querySelector('#todo-list2')
+
+// ADD TASK
+submitBtn2.addEventListener('click', addTask2);
+function addTask2(e) {
+    if (input.value === '') {
+        showAlert2('Please add a task', 'danger');
+    }else{
+        // create list
+        const parentDiv = document.createElement('div')
+        parentDiv.classList = 'd-flex my-3 pb-2 border-bottom justify-content-between'
+        list2.appendChild(parentDiv)
+    
+        const div1 = document.createElement('div')
+        div1.classList = 'd-flex gap-3'
+        div1.innerHTML = '<input type="checkbox" name="checkboxes" class="checkbox2">'
+        const span = document.createElement('span')
+        span.className = 'itemText'
+        span.textContent = input.value
+        div1.appendChild(span)
+        parentDiv.appendChild(div1)
+    
+        const div2 = document.createElement('div')
+        div2.innerHTML = '<i class="fa-regular fa-circle-xmark clearTask2"></i>'
+        parentDiv.appendChild(div2)
+    
+        input.value = ''   
+            
+    }
+    e.preventDefault()
+}
+
+// DELETE TASK
+document.querySelector('.todoList2').addEventListener('click', deleteTask2);
+function deleteTask2(e) {
+    const target = e.target;
+    
+    if (target.classList.contains('clearTask2')) {
+        // Delete the parent task element
+        const task = target.closest('.d-flex');
+        task.remove();
+    }
+}
+
+// STYLING FOR CHECKBOX ON CLICK
+list2.addEventListener('click', function(e) {
+    if (e.target.classList.contains('checkbox2')) {
+        toggleItemText2(e.target);
+    }
+});
+function toggleItemText2(checkbox) {
+    var itemText2 = checkbox.nextElementSibling; 
+    var clearTask2 = checkbox.parentElement.nextElementSibling.querySelector('.clearTask2'); 
+    if (checkbox.checked) {
+        itemText2.classList.add("completed");
+        clearTask2.classList.add('complete');
+    } else {
+        itemText2.classList.remove("completed");
+        clearTask2.classList.remove("complete");
+    }
+}
+
+// ALERT STYLING
+function showAlert2(message, className){
+    const alertDiv = document.createElement('div')
+    alertDiv.className = `alert alert-${className}`
+    alertDiv.append(document.createTextNode(message))
+    todoList2.insertBefore(alertDiv, inputDiv2)
+
+    setTimeout(()=> document.querySelector('.alert').remove(), 2000)
+}
+
+
+// CALENDAR
+const monthYearElement = document.querySelector('#monthYear')
+const datesElement = document.querySelector('#dates')
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+let currentDate = new Date()
+
+// TO SHOW THE CURRENT DATE ON WINDOW LOAD
+document.addEventListener('DOMContentLoaded', function() {
+    currentDate.setMonth(currentDate.getMonth())
+    updateCalendar()
+});
+
+const updateCalendar = () => {
+    const currentYear = currentDate.getFullYear()
+    const currentMonth = currentDate.getMonth()
+
+
+    const firstDay = new Date(currentYear, currentMonth, 0)
+    const lastDay = new Date(currentYear, currentMonth + 1, 0)
+    const totalDays = lastDay.getDate()
+    const firstDayIndex = firstDay.getDay()
+    const lastDayIndex = lastDay.getDay()
+
+    const monthYearString = currentDate.toLocaleDateString
+    ('default', {month: 'long', year: 'numeric'})
+    monthYearElement.textContent = monthYearString
+
+
+    let datesHTML = ''
+
+    for(let i = firstDayIndex; i > 0; i--){
+        const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+
+        datesHTML += `<div class = "date inactive"> ${prevDate.getDate()}</div>`
+    }        
+
+    for(let i = 1; i <= totalDays; i++){
+        const date = new Date(currentYear, currentMonth, i)
+        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+        datesHTML += `<div class="date ${activeClass}"> ${i}</div>`
+    }
+
+    for(let i = 1; i <= 7 - lastDayIndex; i++){
+        const nextDate = new Date(currentYear , currentMonth + 1, i)
+        datesHTML += `<div class="date inactive"> ${nextDate.getDate()}</div>`
+    }
+
+    datesElement.innerHTML = datesHTML
+}
+
+prevBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() - 1)
+    updateCalendar()
+})
+nextBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() + 1)
+    updateCalendar()
+})
+
+// SMALL DEVICES
+const monthYearElement2 = document.querySelector('#monthYear2')
+const datesElement2 = document.querySelector('#dates2')
+const prevBtn2 = document.querySelector('#prevBtn2')
+const nextBtn2 = document.querySelector('#nextBtn2')
+
+let currentDate2 = new Date()
+
+// TO SHOW THE CURRENT DATE ON WINDOW LOAD
+document.addEventListener('DOMContentLoaded', function() {
+    currentDate2.setMonth(currentDate2.getMonth())
+    updateCalendar2()
+});
+
+const updateCalendar2 = () => {
+    const currentYear = currentDate2.getFullYear()
+    const currentMonth = currentDate2.getMonth()
+
+
+    const firstDay = new Date(currentYear, currentMonth, 0)
+    const lastDay = new Date(currentYear, currentMonth + 1, 0)
+    const totalDays = lastDay.getDate()
+    const firstDayIndex = firstDay.getDay()
+    const lastDayIndex = lastDay.getDay()
+
+    const monthYearString = currentDate2.toLocaleDateString
+    ('default', {month: 'long', year: 'numeric'})
+    monthYearElement2.textContent = monthYearString
+
+
+    let datesHTML = ''
+
+    for(let i = firstDayIndex; i > 0; i--){
+        const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+
+        datesHTML += `<div class = "date inactive"> ${prevDate.getDate()}</div>`
+    }        
+
+    for(let i = 1; i <= totalDays; i++){
+        const date = new Date(currentYear, currentMonth, i)
+        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+        datesHTML += `<div class="date ${activeClass}"> ${i}</div>`
+    }
+
+    for(let i = 1; i <= 7 - lastDayIndex; i++){
+        const nextDate = new Date(currentYear , currentMonth + 1, i)
+        datesHTML += `<div class="date inactive"> ${nextDate.getDate()}</div>`
+    }
+
+    datesElement2.innerHTML = datesHTML
+}
+
+prevBtn2.addEventListener('click', () => {
+    currentDate2.setMonth(currentDate2.getMonth() - 1)
+    updateCalendar2()
+})
+nextBtn2.addEventListener('click', () => {
+    currentDate2.setMonth(currentDate2.getMonth() + 1)
+    updateCalendar2()
+})
+
+
+// EXTRA-SMALL DEVICES
+const monthYearElement3 = document.querySelector('#monthYear3')
+const datesElement3 = document.querySelector('#dates3')
+const prevBtn3 = document.querySelector('#prevBtn3')
+const nextBtn3 = document.querySelector('#nextBtn3')
+
+let currentDate3 = new Date()
+
+// TO SHOW THE CURRENT DATE ON WINDOW LOAD
+document.addEventListener('DOMContentLoaded', function() {
+    currentDate3.setMonth(currentDate3.getMonth())
+    updateCalendar3()
+});
+
+const updateCalendar3 = () => {
+    const currentYear = currentDate3.getFullYear()
+    const currentMonth = currentDate3.getMonth()
+
+
+    const firstDay = new Date(currentYear, currentMonth, 0)
+    const lastDay = new Date(currentYear, currentMonth + 1, 0)
+    const totalDays = lastDay.getDate()
+    const firstDayIndex = firstDay.getDay()
+    const lastDayIndex = lastDay.getDay()
+
+    const monthYearString = currentDate3.toLocaleDateString
+    ('default', {month: 'long', year: 'numeric'})
+    monthYearElement3.textContent = monthYearString
+
+
+    let datesHTML = ''
+
+    for(let i = firstDayIndex; i > 0; i--){
+        const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+
+        datesHTML += `<div class = "date inactive"> ${prevDate.getDate()}</div>`
+    }        
+
+    for(let i = 1; i <= totalDays; i++){
+        const date = new Date(currentYear, currentMonth, i)
+        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+        datesHTML += `<div class="date ${activeClass}"> ${i}</div>`
+    }
+
+    for(let i = 1; i <= 7 - lastDayIndex; i++){
+        const nextDate = new Date(currentYear , currentMonth + 1, i)
+        datesHTML += `<div class="date inactive"> ${nextDate.getDate()}</div>`
+    }
+
+    datesElement3.innerHTML = datesHTML
+}
+
+prevBtn3.addEventListener('click', () => {
+    currentDate3.setMonth(currentDate3.getMonth() - 1)
+    updateCalendar3()
+})
+nextBtn3.addEventListener('click', () => {
+    currentDate3.setMonth(currentDate3.getMonth() + 1)
+    updateCalendar3()
+})
